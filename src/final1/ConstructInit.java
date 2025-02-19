a397ce261248826f633676089770c63b84115677
minji <heunhanireum@gmail.com>
2025-02-19 23:39:28 +0900

    section09

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value; //멤버변수에 final 붙으면 생성자 만들때 한번 할당 가능
+
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,13 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10; //static final 쓰면 상수, 대문자로 작성하는 편
+    final int value = 10; //멤버변수에 초기값이 있는 경우 생성자를 통해서도 초기화 불가능
+
+/*
+    public FieldInit(int value) {
+        this.value = value;
+    }
+ */
+}
